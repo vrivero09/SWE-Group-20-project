82df8531ab55be35618b8cb1b6836f08bc5d4dc6
vrivero09 <rivero0288@outlook.com>
2020-01-27 09:22:30 -0500

    test

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+
+  
+
+app.get('/api/customers', (_req, res) => {
+    const customers =[
+        {id:1, firstName: 'Juan', lastName: 'Serret'},
+        {id:2, firstName: 'Vanessa', lastName: 'Serret'},
+        {id:3, firstName: 'Sarah', lastName: 'Whiddon'}
+    ];
+    res.json(customers);
+});
+
+const port = 5000;
+
+app.listen(port, ()=> console.log('Server started on port ${port}'));
+
