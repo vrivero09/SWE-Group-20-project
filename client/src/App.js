0757ae23ef45937eabd7119475334776d17fe760
vrivero09 <rivero0288@outlook.com>
2020-01-27 22:55:41 -0500

    please work

---
+++ client/src/App.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+//import logo from './logo.svg';
+import './App.css';
+import './Nav';
+import './card';
+// import Customers from './component/customers/customers';
+import Nav from './Nav';
+import Card from './card';
+
+class App extends Component {
+  render() {
+    return(
+    <div className="App">
+      <Nav />
+      <div  className ="example">
+      {/* <Customers /> */}
+      <Card />
+      </div>
+      
+    </div>
+ 
+   );
+   
+  }
+  
+}
+
+export default App;
