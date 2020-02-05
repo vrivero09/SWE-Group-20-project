0035e20c61b305b0e31f5315f83ba1686d985126
vrive <vanessariveros25@gmail.com>
2020-02-04 21:03:19 -0500

    Create sign up page and log in page

--- client/src/component/login_registration/home.js
+++ client/src/component/login_registration/home.js
@@ -5,6 +5,8 @@ import {
     Row,Col,Button,
     Modal, ModalHeader, ModalBody, ModalFooter
 } from 'reactstrap';
+import SignUpForm from './signUpForm';
+import LoginForm from './loginForm'
 import Hero from '../../photos/Hero.jpg'
 
 const Login = (props) =>{
@@ -14,18 +16,39 @@ const Login = (props) =>{
     return(
         <div>
             <Button className="w-100" size="md" onClick={toggle}>Log In</Button>
-            <Modal isOpen={modal} toggle={toggle} centered="true">
-                    <ModalHeader toggle={toggle}>Login</ModalHeader>
+            <Modal isOpen={modal} toggle={toggle} centered={true}>
+                <ModalHeader cssModule={{'modal-title': 'w-100 text-center'}} toggle={toggle}>Login</ModalHeader>
                 <ModalBody>
-                    some text here
+                    <LoginForm/>
                 </ModalBody>
                 <ModalFooter>
-                    <Button color="primary" onClick={toggle}>Login</Button>
-                    <Button color="primary" onClick={toggle}>Cancel</Button>
+                    <Button className="w-100" color="primary" onClick={toggle}>Login</Button>
                 </ModalFooter>
             </Modal>
         </div>
     );
+};
+
+const SignUp = (props) => {
+    const [modal, setModal] = useState(false);
+    const toggle = () => setModal(!modal);
+
+    return(
+        <div>
+            <Button className="w-100" color="info" size="md" onClick={toggle}>Sign Up</Button>
+            <Modal isOpen={modal} toggle={toggle} centered={true} scrollable={true} size="lg">
+                <ModalHeader cssModule={{'modal-title': 'w-100 text-center'}} toggle={toggle}>Sign Up</ModalHeader>
+                <ModalBody className="m-3">
+                    <p className="text-center">Welcome to GeekText! Please enter the information below and click submit to sign up.</p>
+                    <SignUpForm></SignUpForm>
+                </ModalBody>
+                <ModalFooter>
+                    <Button className="w-100" color="info" onClick={toggle}>Submit</Button>
+                </ModalFooter>
+            </Modal>
+        </div>
+    );
+
 };
 
 const Home = (props) =>{
@@ -38,7 +61,7 @@ const Home = (props) =>{
                     <CardText className="box" tag="h4">Welcome to GeekText where you can browse thousands of books and read them on your free time.</CardText>
                     <Row className="box btns">
                         <Col  sm='6'>
-                            <Button color="info" className="w-100" size="md">Sign Up</Button>
+                            <SignUp/>
                         </Col>
                         <Col  sm='6'>
                             <Login/>
