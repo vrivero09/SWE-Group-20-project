b8a6c5919a3be573f322dbb9e9286f28a4c8ed0c
vrive <vanessariveros25@gmail.com>
2020-02-04 19:12:46 -0500

    Add login modal

--- client/src/component/login_registration/home.js
+++ client/src/component/login_registration/home.js
@@ -2,14 +2,37 @@ import React, { useState } from 'react';
 import './home.css'
 import {
     Card, CardImg, CardImgOverlay, CardText, CardTitle,
-    Row,Col,Button
+    Row,Col,Button,
+    Modal, ModalHeader, ModalBody, ModalFooter
 } from 'reactstrap';
+import Hero from '../../photos/Hero.jpg'
+
+const Login = (props) =>{
+    const [modal, setModal] = useState(false);
+    const toggle = () => setModal(!modal);
+
+    return(
+        <div>
+            <Button className="w-100" size="md" onClick={toggle}>Log In</Button>
+            <Modal isOpen={modal} toggle={toggle} centered="true">
+                    <ModalHeader toggle={toggle}>Login</ModalHeader>
+                <ModalBody>
+                    some text here
+                </ModalBody>
+                <ModalFooter>
+                    <Button color="primary" onClick={toggle}>Login</Button>
+                    <Button color="primary" onClick={toggle}>Cancel</Button>
+                </ModalFooter>
+            </Modal>
+        </div>
+    );
+};
 
 const Home = (props) =>{
     return(
         <div>
             <Card style={{border: "none"}} className="text-center" inverse>
-                <CardImg width="100%" src="https://video-images.vice.com/articles/5d44c9622980b0000824a7e3/lede/1564789576071-GettyImages-949118068.jpeg?crop=1xw%3A0.8419xh%3B0xw%2C0.1581xh&resize=2000%3A*" alt="Card image cap" />
+                <CardImg width="100%" src={Hero} alt="Card image cap" />
                 <CardImgOverlay className="content">
                     <CardTitle style={{fontSize: '5rem'}} >GeekText</CardTitle>
                     <CardText className="box" tag="h4">Welcome to GeekText where you can browse thousands of books and read them on your free time.</CardText>
@@ -18,7 +41,7 @@ const Home = (props) =>{
                             <Button color="info" className="w-100" size="md">Sign Up</Button>
                         </Col>
                         <Col  sm='6'>
-                            <Button className="w-100" size="md">Log In</Button>
+                            <Login/>
                         </Col>
                     </Row>
                 </CardImgOverlay>
