7727dfba60c7179ed2914352a4cef7775a633053
vrive <vrive072@fiu.edu>
2020-02-10 18:12:11 -0500

    remove loginForm references

--- client/src/App.js
+++ client/src/App.js
@@ -6,11 +6,9 @@ import {BrowserRouter, Route} from 'react-router-dom'
 import Home from './component/Home';
 import Cart from './component/Cart';
 import Product from './component/Product';
-import signUpForm from './component/login_registration/signUpForm';
 import landing from './component/login_registration/landing';
-import LoginForm from './component/login_registration/loginForm';
-//import Forms from './component/Form';
-import { Form } from 'reactstrap';
+import Form from './component/Form';
+//import { Form } from 'reactstrap';
 
 
 class App extends Component{
@@ -22,8 +20,6 @@ class App extends Component{
       <Route exact path ='/' component={Home} />
       <Route exact path ='/Cart' component={Cart} />
       <Route exact path ='/Products' component={Product} />
-      <Route exact path ='/SignUp' component={signUpForm} />
-      <Route exact path ='/Login' component={LoginForm} />
       <Route exact path ='/Profile' component={landing} />
       <Route exact path ='/Ratings' component={Form} />
       
