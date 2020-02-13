151109933c65e35ceab8dcd5feacb0749da0663c
Vanessa Serret <rivero0288@outlook.com>
2020-02-12 22:50:55 -0500

    commit

--- client/src/component/Cart.js
+++ client/src/component/Cart.js
@@ -1,91 +1,21 @@
 import React, {Component} from 'react';
-import { Provider } from "react-redux";
-import { createStore, combineReducers } from "redux";
-import {
-  Cart,
-  Product,
-  CheckoutButton,
-  cartLocalization,
-  cartReducer,
-  setCartCurrency
-} from "react-shopping-cart";
- 
-import "bootstrap/dist/css/bootstrap.css";
-import "animate.css/animate.min.css";
-import "font-awesome/css/font-awesome.min.css";
- 
+import productList from './shoppingCart/productList';
+//import Card from './card/card';
 
-class shopping extends Component {
-  state = {
-    product: {
-      name: "iPadCase",
-      id: "ipad-case",
-      path: "/shop/ipad-case/",
-      properties: {
-        color: [
-          "red",
-          "green",
-          {
-            additionalCost: {
-              GBP: 1,
-              EUR: 2,
-              USD: 3.5
-            },
-            value: "yellow"
-          }
-        ]
-      },
-      propertiesToShowInCart: ["color"],
-      prices: { GBP: 70, EUR: 80, USD: 90 },
-      currency: "GBP",
-      imageSrc: "1-483x321.jpeg"
-    },
-    getProductLocalization: getDefaultLocalization("product", "en", {
-      ...iPadCaseLocalization,
-      ...iPadPropertiesWithAdditionalCostLocalization
-    }),
-    getCheckoutButtonLocalization: getDefaultLocalization(
-      "checkoutButton",
-      "en",
-      iPadCaseLocalization
-    ),
-    getCartLocalization: getDefaultLocalization(
-      "cart",
-      "en",
-      iPadCaseLocalization
-    )
-  };
- 
-  render() {
-    const {
-      product,
-      getCheckoutButtonLocalization,
-      getProductLocalization,
-      getCartLocalization 
-    } = this.state;
- 
-    const checkoutButtonElement = (
-      <CheckoutButton
-        getLocalization={getCheckoutButtonLocalization}
-        checkoutURL="/to/my/checkout"
-      />
-    );
-    return (
-      <Provider store={store}>
-        <div className="container">
-          <Product
-            {...product}
-            checkoutButton={checkoutButtonElement}
-            getLocalization={getProductLocalization}
-          />
-          <Cart
-            checkoutButton={checkoutButtonElement}
-            getLocalization={getCartLocalization}
-          />
+class Cart extends Component {
+    render() {
+      return(
+      <div className="Add to Cart">
+        <div  className ="example">
+            <h1>Cart Page</h1>
         </div>
-      </Provider>
-    );
+      </div>
+      
+   
+     );
+     
+    }
+    
   }
-}
- 
-export default shopping;
+  
+  export default Cart;
