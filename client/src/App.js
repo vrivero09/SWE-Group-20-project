ece66a4c9acd8e4e391fdb59d3b09b6d2e5b8009
Vanessa Serret <rivero0288@outlook.com>
2020-02-13 14:34:38 -0500

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ import landing from './component/login_registration/landing';
 import LoginForm from './component/login_registration/loginForm';
 import Form from './component/Form';
 import Product from './component/shoppingCart/Product';
-import productItem from './component/shoppingCart/productItem';
+//import productItem from './component/shoppingCart/productItem';
 
 
 
@@ -35,4 +35,12 @@ class App extends Component{
   );
 }
 }
+
+let cards =[
+  {'name': 'HP 1', 'id': 1},
+  {'name': 'HP 2', 'id': 2},
+  {'name': 'HP 3', 'id': 3},
+  {'name': 'Star wars', 'id': 4},
+];
+
 export default App;
