ab4842dc3dbeafed3eec1ff07cbb38e13e2b2acd
Vanessa Serret <rivero0288@outlook.com>
2020-02-13 13:02:14 -0500

    edited js file name

--- client/src/component/shoppingCart/productITem.js
+++ client/src/component/shoppingCart/productITem.js
@@ -1,5 +1,5 @@
 import React from 'react';
-    import ProductItem from './ProductItem';
+    import ProductItem from './productItem';
     import { getProducts } from '../repository';
     import { Link } from 'react-router-dom';
     
