988f46c27a99db0bd85a3d950498d9f9908fc50c
Vanessa Serret <rivero0288@outlook.com>
2020-02-13 23:22:43 -0500

    commit

--- client/src/component/shoppingCart/Cart.js
+++ client/src/component/shoppingCart/Cart.js
@@ -1,21 +1,60 @@
-import React, {Component} from 'react';
-// import productList from './shoppingCart/productList';
-//import Card from './card/card';
 
-class Cart extends Component {
-    render() {
-      return(
-      <div className="Add to Cart">
-        <div  className ="example">
-            <h1>Cart Page</h1>
-        </div>
-      </div>
-      
-   
-     );
-     
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
+class Cart extends Component{
+
+    render(){
+              
+        let addedItems = this.props.items.length ?
+            (  
+                this.props.items.map(item=>{
+                    return(
+                       
+                        <li className="collection-item avatar" key={item.id}>
+                                    <div className="item-img"> 
+                                        <img src={item.img} alt={item.img} className=""/>
+                                    </div>
+                                
+                                    <div className="item-desc">
+                                        <span className="title">{item.title}</span>
+                                        <p>{item.desc}</p>
+                                        <p><b>Price: {item.price}$</b></p> 
+                                        <p>
+                                            <b>Quantity: {item.quantity}</b> 
+                                        </p>
+                                        <div className="add-remove">
+                                            <Link to="/cart"><i className="material-icons">arrow_drop_up</i></Link>
+                                            <Link to="/cart"><i className="material-icons">arrow_drop_down</i></Link>
+                                        </div>
+                                        <button className="waves-effect waves-light btn pink remove">Remove</button>
+                                    </div>
+                                    
+                               </li>                        
+                    )
+                })
+            ):
+
+             (
+                <p>Nothing.</p>
+             )
+       return(
+            <div className="container">
+                <div className="cart">
+                    <h5>You have ordered:</h5>
+                    <ul className="collection">
+                        {addedItems}
+                    </ul>
+                </div>  
+            </div>
+       )
     }
-    
-  }
-  
-  export default Cart;
+}
+
+const mapStateToProps = (state)=>{
+    return{
+        items: state.addedItems
+    }
+}
+
+export default connect(mapStateToProps)(Cart)
--- client/src/component/shoppingCart/reducer/cartReducer.js
+++ client/src/component/shoppingCart/reducer/cartReducer.js
@@ -1,23 +1,49 @@
-import HP1 from './photo/'
-import HP22 from './photos/HP22';
-import HP33 from './photos/HP33';
-import SW11 from './photos/SW11';
-
+import HP1 from './photo/HP1.jpg'
+import HP22 from './photo/HP22.jpg';
+import HP33 from './photo/HP33.jpg';
+import SW11 from './photo/SW11.jpg';
+import { ADD_TO_CART,REMOVE_ITEM,SUB_QUANTITY,ADD_QUANTITY,ADD_SHIPPING } from '../actions/action-types/cart-actions'
 
 const initState = {
     items: [
-        {id:1,title:'Winter body', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price:110,img:HP1},
-        {id:2,title:'Adidas', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price:80,img: HP22},
-        {id:3,title:'Vans', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.",price:120,img: HP33},
-        {id:4,title:'White', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price:260,img:SW11}
+        {id:1,title:'Deathly Hallows', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price:20,img:HP1},
+        {id:2,title:'Goblet of Fire', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price:30,img: HP22},
+        {id:3,title:'Deathly Hallows: Old version', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.",price:15,img: HP33},
+        {id:4,title:'Star Wars: Darth Vader', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price:50,img:SW11}
     ],
     addedItems:[],
     total: 0
 
 }
 const cartReducer= (state = initState,action)=>{
-    
+    //INSIDE HOME COMPONENT
+    if(action.type === ADD_TO_CART){
+        let addedItem = state.items.find(item=> item.id === action.id)
+        //check if the action id exists in the addedItems
+       let existed_item= state.addedItems.find(item=> action.id === item.id)
+       if(existed_item)
+       {
+          addedItem.quantity += 1 
+           return{
+              ...state,
+               total: state.total + addedItem.price 
+                }
+      }
+       else{
+          addedItem.quantity = 1;
+          //calculating the total
+          let newTotal = state.total + addedItem.price 
+          
+          return{
+              ...state,
+              addedItems: [...state.addedItems, addedItem],
+              total : newTotal
+          }
+          
+      }
+  }
+  else{
     return state;
-
+  }
 }
 export default cartReducer;
