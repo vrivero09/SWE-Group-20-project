383341c112f3354cd306160cf1248ad593f5f858
vrive <vrive072@fiu.edu>
2020-02-22 00:24:57 -0500

    add comments to book model/routes

--- server/routes/Books.js
+++ server/routes/Books.js
@@ -1,59 +1,49 @@
 const express = require("express");
 var cors = require("cors");
 const books = express.Router();
+const mongoose = require("mongoose");
+
+/*
+  see https://mongoosejs.com/docs/models.html for querying, deleting, and updating documents
+*/
 
 //get book model
 const Book = require("../models/Book");
 books.use(cors());
 
-//endpont to add user to database
+//endpoint to add review to database
 books.post("/addreview", (req, res) => {
-  //must validate confirm password! TODO
+
+  //store information from request body that will be used to create a new review
   const reviewData = {
     reviewText: req.body.reviewText,
     starRating: req.body.starRating,
     showUsername: req.body.showUsername
   };
 
-  const book_ID = req.body._id;
+  //store book id that was given in request body, need to cast the string into ObjectId
+  const book_ID = new mongoose.Types.ObjectId(req.body._id);
+
+  //use a model method to query the database, find the book with the given id
   Book.findOne({
     _id: book_ID
   }).then(book => {
+    //.then is a callback function that lets you do something with the result of the query
+    //in this case, the result is stored in book variable
     if (book == null) {
-      console.log("BOOK IS NULL.");
+      res.send("Book is null");
     } else {
+      //if found a book with given id, push the new review to the allReviews array inside the book document
       book.allReviews.push(reviewData);
+      //save and update in the database
       book.save();
+
+      res.send("Review saved");
     }
   });
 
-  //find user by id (username)
-  User.findOne({
-    _id: req.body.username
-  })
-    .then(user => {
-      //new user
-      if (!user) {
-        bcrypt.hash(req.body.password, 10, (err, hash) => {
-          //make user password in db the hashed version
-          userData.password = hash;
-          //create user using userData from request
-          User.create(userData)
-            .then(user => {
-              res.json({ status: user._id + " registered" });
-            })
-            .catch(err => {
-              res.send("error: " + err);
-            });
-        });
-      } else {
-        //user already exists
-        res.json({ error: "User already exists" });
-      }
-    })
-    .catch(err => {
-      res.send("error: " + err);
-    });
 });
 
+//must export your routes so you can use them in server.js
 module.exports = books;
+
