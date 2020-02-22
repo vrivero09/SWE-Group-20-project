d44bba92704cffa75c00f42d16c7e5324df1bbb0
Alejandro <aramo173@fiu.edu>
2020-02-21 20:11:19 -0500

    review system now links to monogodb

--- server/server.js
+++ server/server.js
@@ -1,16 +1,16 @@
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const express = require('express');
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const port = 5000; 
+const port = 5000;
 app.use(bodyParser.json());
 app.use(cors());
 app.use(
-    bodyParser.urlencoded({
-        extended:false
-    })
+  bodyParser.urlencoded({
+    extended: false
+  })
 );
 
 // app.get('/api/customers', (_req, res) => {
@@ -22,23 +22,26 @@ app.use(
 //     res.json(customers);
 // });
 
+const mongoURI =
+  "mongodb+srv://admin:admin123@cluster0-ywzdx.mongodb.net/test?retryWrites=true&w=majority";
 
-const mongoURI = 'mongodb+srv://admin:admin123@cluster0-ywzdx.mongodb.net/test?retryWrites=true&w=majority';
-
-mongoose.connect(mongoURI, {useUnifiedTopology: true, useNewUrlParser: true})
-    .catch(err => console.log(err));
+mongoose
+  .connect(mongoURI, { useUnifiedTopology: true, useNewUrlParser: true })
+  .catch(err => console.log(err));
 
 const connection = mongoose.connection;
 
-connection.once('open', function(){
-    console.log("MongoDB database conncetion established successfully");
+connection.once("open", function() {
+  console.log("MongoDB database conncetion established successfully");
 });
 
+var Books = require("./routes/Books");
+app.use("/book", Books);
 
 //import user route
-var Users = require('./routes/Users');
+var Users = require("./routes/Users");
 
 //use the route
-app.use('/users', Users);
+app.use("/users", Users);
 
-app.listen(port, ()=> console.log('Server started on port ' + port));
+app.listen(port, () => console.log("Server started on port " + port));
