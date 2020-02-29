81cabf78e5fd9a9ad034d0fede1a471e7cb6a841
Vanessa Serret <rivero0288@outlook.com>
2020-02-29 11:16:16 -0500

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -5,11 +5,11 @@ import Navigation from './component/Nav';
 import {BrowserRouter, Route} from 'react-router-dom'
 import Home from './component/Home';
 import Cart from './component/shoppingCart/Cart';
-import Product from './component/shoppingCart/Product';
+//import Product from './component/shoppingCart/Product';
 import Landing from './component/login_registration/landing';
 import Form from './component/Form';
 import Profile from './component/profile/profile'
-
+import Card from './component/shoppingCart/card/card'
 
 class App extends Component{
   constructor(props){
@@ -40,7 +40,7 @@ class App extends Component{
       <Route exact path ='/' render={(props) => <Landing {...props} logIn={this.logIn} />}/>
       <Route path ='/Home' component={Home} />
       <Route path ='/Cart' component={Cart} />
-      <Route path ='/Products' component={Product} />
+      <Route path ='/Products' component={Card} />
       <Route path ='/Ratings' component={Form} />
       <Route path ='/Profile' component={Profile} />
       
--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,14 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import cartReducer from './component/shoppingCart/reducer/cartReducer'
 //import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
 
+const store = createStore(cartReducer);
 
-ReactDOM.render(<App/>, document.getElementById('root'));
-
-//ReactDOM.render(<Landing />, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
 
 
