45a2cef1998489576f94bb7cd2fa30910afa3f6e
Vanessa Serret <rivero0288@outlook.com>
2020-03-03 14:03:46 -0500

    commit

--- client/src/component/shoppingCart/Cart.js
+++ client/src/component/shoppingCart/Cart.js
@@ -1,86 +1,162 @@
+import React, { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux';
+import {
+    getCartItems,
+    removeCartItem,
+    onSuccessBuy
+} from './actions/user_actions';
+//import UserCardBlock from './Sections/UserCardBlock';
+import { Result, Empty } from 'antd';
+import Axios from 'axios';
+//import Paypal from '../../utils/Paypal';
 
-import React, { Component } from 'react';
-import { connect } from 'react-redux'
-import { Link } from 'react-router-dom'
-import { removeItem,addQuantity,subtractQuantity} from './actions/action-types/cartActions';
-import Checkout from './checkOut';
-//import item from './productItem'
+function Cart(props) {
+    const dispatch = useDispatch();
+    const [Total, setTotal] = useState(0)
+    const [ShowTotal, setShowTotal] = useState(false)
+    const [ShowSuccess, setShowSuccess] = useState(false)
+
+    useEffect(() => {
+
+        let cartItems = [];
+        if (props.user.userData && props.user.userData.cart) {
+            if (props.user.userData.cart.length > 0) {
+                props.user.userData.cart.forEach(item => {
+                    cartItems.push(item.id)
+                });
+                dispatch(getCartItems(cartItems, props.user.userData.cart))
+
+            }
+        }
+
+    }, [props.user.userData, dispatch])
+
+    useEffect(() => {
+
+        if (props.user.cartDetail && props.user.cartDetail.length > 0) {
+            calculateTotal(props.user.cartDetail)
+        }
 
 
+    }, [props.user.cartDetail])
 
-class Cart extends Component{
-       //to remove the item completely
-       handleRemove = (id)=>{
-        this.props.removeItem(id);
+    const calculateTotal = (cartDetail) => {
+        let total = 0;
+
+        cartDetail.map(item => {
+            total += parseInt(item.price, 10) * item.quantity
+        });
+
+        setTotal(total)
+        setShowTotal(true)
     }
-    //to add the quantity
-    handleAddQuantity = (id)=>{
-        this.props.addQuantity(id);
-    }
-    //to substruct from the quantity
-    handleSubtractQuantity = (id)=>{
-        this.props.subtractQuantity(id);
+
+
+    const removeFromCart = (productId) => {
+
+        dispatch(removeCartItem(productId))
+            .then(() => {
+
+                Axios.get('/api/users/userCartInfo')
+                    .then(response => {
+                        if (response.data.success) {
+                            if (response.data.cartDetail.length <= 0) {
+                                setShowTotal(false)
+                            } else {
+                                calculateTotal(response.data.cartDetail)
+                            }
+                        } else {
+                            alert('Failed to get cart info')
+                        }
+                    })
+            })
     }
 
-    render(){         
-        let addedItems = this.props.items.length ?
-            (  
-                this.props.items.map(item=>{
-                    return(
-                       
-                        <li className="collection-item avatar" key={item.id}>
-                                    <div className="item-img"> 
-                                        <img src={item.img} alt={item.img}/>
-                                    </div>
-                                
-                                    <div className="item-desc">
-                                        <span className="title">{item.title}</span>
-                                        <p>{item.desc}</p>
-                                        <p><b>Price: ${item.price}</b></p> 
-                                        <p>
-                                            <b>Quantity: {item.quantity}</b> 
-                                        </p>
-                                        <div className="add-remove">
-                                            <Link to="/cart"><i className="material-icons"  onClick={()=>{this.handleAddQuantity(item.id)}}>arrow_drop_up</i></Link>
-                                            <Link to="/cart"><i className="material-icons" onClick={()=>{this.handleSubtractQuantity(item.id)}}>arrow_drop_down</i></Link>
-                                        </div>
-                                        <button className="waves-effect waves-light btn pink remove" onClick={()=>{this.handleRemove(item.id)}}>Remove</button>
-                                    </div>
-                                    
-                               </li>                        
-                    )
-                })
-            ):
+    // const transactionSuccess = (data) => {
 
-             (
-                <p>Nothing.</p>
-             )
-       return(
-            <div className="container">
-                <div className="cart">
-                    <h5>You have ordered:</h5>
-                    <ul className="collection">
-                        {addedItems}
-                    </ul>
-                    </div> 
-                <Checkout />          
+    //     let variables = {
+    //         cartDetail: props.user.cartDetail, paymentData: data
+    //     }
+
+    //     Axios.post('/api/users/successBuy', variables)
+    //         .then(response => {
+    //             if (response.data.success) {
+    //                 setShowSuccess(true)
+    //                 setShowTotal(false)
+
+    //                 dispatch(onSuccessBuy({
+    //                     cart: response.data.cart,
+    //                     cartDetail: response.data.cartDetail
+    //                 }))
+
+    //             } else {
+    //                 alert('Failed to buy it')
+    //             }
+    //         })
+
+    // }
+
+    const transactionError = () => {
+        console.log('Paypal error')
+    }
+
+    const transactionCanceled = () => {
+        console.log('Transaction canceled')
+    }
+
+
+    return (
+        <div style={{ width: '85%', margin: '3rem auto' }}>
+            <h1>My Cart</h1>
+            <div>
+
+                { <UserCardBlock
+                    products={props.user.cartDetail}
+                    removeItem={removeFromCart}
+                />
+ }
+
+                {ShowTotal ?
+                    <div style={{ marginTop: '3rem' }}>
+                        <h2>Total amount: ${Total} </h2>
+                    </div>
+                    :
+                    ShowSuccess ?
+                        <Result
+                            status="success"
+                            title="Successfully Purchased Items"
+                        /> :
+                        <div style={{
+                            width: '100%', display: 'flex', flexDirection: 'column',
+                            justifyContent: 'center'
+                        }}>
+                            <br />
+                            <Empty description={false} />
+                            <p>No Items In the Cart</p>
+
+                        </div>
+                }
             </div>
-       )
-    }
+
+
+
+            {/* Paypal Button */}
+
+            {/* {ShowTotal &&
+
+                <Paypal
+                    toPay={Total}
+                    onSuccess={transactionSuccess}
+                    transactionError={transactionError}
+                    transactionCanceled={transactionCanceled}
+                />
+
+            } */}
+
+
+
+        </div>
+    )
 }
 
-const mapStateToProps = (state)=>{
-    return{
-        items: state.addedItems
-    }
-}
-
-// const mapDispatchToProps = (dispatch)=>{
-//     return{
-//         removeItem: (id)=>{dispatch(removeItem(id))},
-//         addQuantity: (id)=>{dispatch(addQuantity(id))},
-//         subtractQuantity: (id)=>{dispatch(subtractQuantity(id))}
-//     }
-// }
-
-export default connect(mapStateToProps)(Cart)
+export default Cart
