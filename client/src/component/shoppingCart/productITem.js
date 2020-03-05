2a96648eaec7809c15b6691a56c225eabe15042a
Vanessa Serret <rivero0288@outlook.com>
2020-03-05 08:44:46 -0500

    commit

--- client/src/component/shoppingCart/productITem.js
+++ client/src/component/shoppingCart/productITem.js
@@ -1,52 +1,102 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, {Component} from 'react';
 import { connect } from 'react-redux'
-import getProduct from './Product'
-import { addToCart } from './actions/action-types/cartActions';
-//import card from './card/card';
-    
-  class productItem extends React.Component {
-         constructor(props) {
-         super(props);
-         this.state = {
-           product: []
-           }
-        }
-      
-      componentDidMount() {
-      getProduct().then((product) =>this.setState({ product }));
-      }
-      
-        render() {
-          const { product } =  this.state;
-          return (
-            <div className=" container">
-              <h3 className="card-title">List of Available Products</h3><hr/>
-            {product.map((product, index) => <productItem product={product} key={index}/>)}
-              <hr/>
-              <Link to="/checkout">
-                <button className="btn btn-success float-right">Checkout</button>
-              </Link>
-              <Link to="/cart">
-                <button className="btn btn-primary float-right" 
-                    style={{  marginRight: "10px" }}>View Cart</button>
-              </Link><br/><br/><br/>
-            </div>
-          );
-        }
-      }
+import { addToCart, removeItem } from './actions/action-types/cartActions';
+import Checkout from './checkOut'
+import {
+  Card, CardText, CardBody,CardImg,
+  CardTitle, CardSubtitle, Container, Button, Row, Col
+} from 'reactstrap';
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
 
-    const mapStateToProps = (state)=>{
-      return {
-          items: state.items
-           }
-      }
-    const mapDispatchToProps= (dispatch)=>{
-        
-    return{
-      addToCart: (id)=>{dispatch(addToCart(id))}
-      }
+
+
+//import Book from 'C:\Users\river\OneDrive\Documents\GitHub\SWE-Group-20-project\server\models\Book.js'
+
+// const schema= new this.schema({
+// bookTitle:{type: String, required:true},
+// price:{type: Number, required:true},
+// author:{type: String, required:true},
+// authorBio:{type: String, required:true},
+// averageRating:{type: Number, required:true},
+// description:{type: String, required:true},
+// genre:{type: String, required:true},
+// publisher:{type: String, required:true},
+// bookImage:{type: String, required:true}
+
+// });
+//module.exports = mongoose.model('Product', Book);
+
+
+class Product extends Component {
+
+  constructor(props){
+    super(props);
+    this.state={
+      total:100
     }
-     
-     
-    export default connect(mapStateToProps, mapDispatchToProps)(productItem)
+  }
+
+  handleClick = (id)=>{
+    this.props.addToCart(id); 
+}
+
+handleRemove = (id)=>{
+  this.props.removeItem(id);
+}
+
+  render() {
+    let itemList = this.props.items.map(item=>{
+      return(
+        <Container className="container">
+        <div >
+      <Card className ="purchase-card" style={{width:"49%", height:"40%"}}>
+          <CardImg src={item.img} alt={item.title} fluid/>
+          <CardBody>
+            <CardTitle>Card title</CardTitle>
+            <CardSubtitle>{item.title}</CardSubtitle>
+            <CardText><b>Price: ${item.price}</b></CardText>
+              <Link to="/Cart"><Button onClick={()=>{this.handleClick(item.id)}}><AddShoppingCartIcon/></Button></Link>
+              <Link to="/Cart"><Button onClick={()=>{this.handleRemove(item.id)}}>Remove</Button></Link>
+          </CardBody>
+        </Card>
+        </div>
+        </Container>
+       
+      )
+  })
+  return(
+    <div className="container_cards">
+          <h3 className="center">Our products</h3>
+          <Checkout href="/Cart" price={this.state.total} item={this.props.items} /> 
+        <Container className="items">
+        <div className="col-xs-6">
+        <Col gutter ={[3 ,3]}>
+          {itemList}
+        </Col>  
+        </div>
+        </Container>   
+    </div>    
+        );
+  }
+}
+const mapStateToProps = (state)=>{
+  return {
+      items: state.items
+       }
+  }
+const mapDispatchToProps= (dispatch)=>{
+    
+return{
+  addToCart: (id)=>{dispatch(addToCart(id))},
+  removeItem: (id)=>{dispatch(removeItem(id))}
+  // addQuantity: (id)=>{dispatch(addQuantity(id))},
+  // subtractQuantity: (id)=>{dispatch(subtractQuantity(id))}
+  }
+}
+
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(Product);
+
+
+
