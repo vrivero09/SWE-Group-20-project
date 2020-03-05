ae86856a14f0473b696c61879363d8cfee70ffae
Vanessa Serret <rivero0288@outlook.com>
2020-03-05 07:40:04 -0500

    commit

--- client/src/component/shoppingCart/Cart.js
+++ client/src/component/shoppingCart/Cart.js
@@ -1,20 +1,86 @@
-import React, {Component} from 'react';
-//import Card from './card/card';
 
-class Cart extends Component {
-    render() {
-      return(
-      <div className="Add to Cart">
-        <div  className ="example">
-            <h1>Cart Page</h1>
-        </div>
-        
-      </div>
-   
-     );
-     
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
+import { removeItem,addQuantity,subtractQuantity} from './actions/action-types/cartActions';
+import Checkout from './checkOut';
+//import item from './productItem'
+
+
+
+class Cart extends Component{
+       //to remove the item completely
+       handleRemove = (id)=>{
+        this.props.removeItem(id);
     }
-    
-  }
-  
-  export default Cart;
+    //to add the quantity
+    handleAddQuantity = (id)=>{
+        this.props.addQuantity(id);
+    }
+    //to substruct from the quantity
+    handleSubtractQuantity = (id)=>{
+        this.props.subtractQuantity(id);
+    }
+
+    render(){         
+        let addedItems = this.props.items.length ?
+            (  
+                this.props.items.map(item=>{
+                    return(
+                       
+                        <li className="collection-item avatar" key={item.id}>
+                                    <div className="item-img"> 
+                                        <img src={item.img} alt={item.img}/>
+                                    </div>
+                                
+                                    <div className="item-desc">
+                                        <span className="title">{item.title}</span>
+                                        <p>{item.desc}</p>
+                                        <p><b>Price: ${item.price}</b></p> 
+                                        <p>
+                                            <b>Quantity: {item.quantity}</b> 
+                                        </p>
+                                        <div className="add-remove">
+                                            <Link to="/cart"><i className="material-icons"  onClick={()=>{this.handleAddQuantity(item.id)}}>arrow_drop_up</i></Link>
+                                            <Link to="/cart"><i className="material-icons" onClick={()=>{this.handleSubtractQuantity(item.id)}}>arrow_drop_down</i></Link>
+                                        </div>
+                                        <button className="waves-effect waves-light btn pink remove" onClick={()=>{this.handleRemove(item.id)}}>Remove</button>
+                                    </div>
+                                    
+                               </li>                        
+                    )
+                })
+            ):
+
+             (
+                <p>Nothing.</p>
+             )
+       return(
+            <div className="container">
+                <div className="cart">
+                    <h5>You have ordered:</h5>
+                    <ul className="collection">
+                        {addedItems}
+                    </ul>
+                    </div> 
+                <Checkout />          
+            </div>
+       )
+    }
+}
+
+const mapStateToProps = (state)=>{
+    return{
+        items: state.addedItems
+    }
+}
+
+// const mapDispatchToProps = (dispatch)=>{
+//     return{
+//         removeItem: (id)=>{dispatch(removeItem(id))},
+//         addQuantity: (id)=>{dispatch(addQuantity(id))},
+//         subtractQuantity: (id)=>{dispatch(subtractQuantity(id))}
+//     }
+// }
+
+export default connect(mapStateToProps)(Cart)
