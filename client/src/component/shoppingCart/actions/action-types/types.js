17d81db87dfd3fe9a8cc67363cdc914c41ac8c4b
Vanessa Serret <rivero0288@outlook.com>
2020-03-05 06:57:41 -0500

    putting back original action folder

--- client/src/component/shoppingCart/actions/action-types/types.js
+++ client/src/component/shoppingCart/actions/action-types/types.js
@@ -1,4 +1,3 @@
-//Types should be in const to avoid typos and duplication since it's a string and could be easily miss spelled
 export const ADD_TO_CART = 'ADD_TO_CART';
 export const REMOVE_ITEM = 'REMOVE_ITEM';
 export const SUB_QUANTITY = 'SUB_QUANTITY';
