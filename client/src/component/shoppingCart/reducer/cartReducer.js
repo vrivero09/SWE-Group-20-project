612cbe59f9e2549128a29c2199be5aba1c7553a3
Vanessa Serret <rivero0288@outlook.com>
2020-03-05 07:04:04 -0500

    commit

--- client/src/component/shoppingCart/reducer/cartReducer.js
+++ client/src/component/shoppingCart/reducer/cartReducer.js
@@ -1,18 +1,26 @@
-import { ADD_TO_CART,REMOVE_ITEM,SUB_QUANTITY,ADD_QUANTITY } from '../actions/types'
-
-
+import Item1 from '../../images/item1.jpg'
+import Item2 from '../../images/item2.jpg'
+import Item3 from '../../images/item3.jpg'
+import Item4 from '../../images/item4.jpg'
+import Item5 from '../../images/item5.jpg'
+import Item6 from '../../images/item6.jpg'
+import { ADD_TO_CART,REMOVE_ITEM,SUB_QUANTITY,ADD_QUANTITY,ADD_SHIPPING } from '../actions/action-types/types'
 
 
 const initState = {
-    items: [],
-    addedItem:[],
+    items: [
+        {id:1,title:'Winter body', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price:110,img:Item1},
+        {id:2,title:'Adidas', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price:80,img: Item2},
+        {id:3,title:'Vans', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.",price:120,img: Item3},
+        {id:4,title:'White', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price:260,img:Item4},
+        {id:5,title:'Cropped-sho', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price:160,img: Item5},
+        {id:6,title:'Blues', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.",price:90,img: Item6}
+    ],
+    addedItems:[],
     total: 0
 
 }
-
-
 const cartReducer= (state = initState,action)=>{
-
    
     //INSIDE HOME COMPONENT
     if(action.type === ADD_TO_CART){
@@ -85,7 +93,25 @@ const cartReducer= (state = initState,action)=>{
         }
         
     }
+
+    if(action.type=== ADD_SHIPPING){
+          return{
+              ...state,
+              total: state.total + 6
+          }
+    }
+
+    if(action.type=== 'SUB_SHIPPING'){
+        return{
+            ...state,
+            total: state.total - 6
+        }
+  }
+    
+  else{
     return state
+    }
+    
 }
 
 export default cartReducer
