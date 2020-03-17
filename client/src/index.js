89df67e859c26ad8b05b584e92d3102204624c81
Vanessa Serret <rivero0288@outlook.com>
2020-03-16 20:50:12 -0400

    fixed conflicts

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-<<<<<<< HEAD
-// import { Provider } from 'react-redux';
-// import cartReducer from './component/shoppingCart/reducer/cartReducer'
-//import * as serviceWorker from './serviceWorker';
-// import { createStore } from 'redux';
-
-// const store = createStore(cartReducer);
-
-// ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
-ReactDOM.render(<App />, document.getElementById('root'));
-=======
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -20,7 +9,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 // import { createStore } from 'redux';
 
 //const store = createStore(cartReducer);
->>>>>>> shoppingCart
 
 //ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 ReactDOM.render(<App />, document.getElementById('root'));
