8892e964357259bc287ef61aca76d64b916bd30c
Vanessa Serret <rivero0288@outlook.com>
2020-03-19 15:44:43 -0400

    finally got something going for the Shopping Cart

--- client/src/component/shoppingCart/productITem.js
+++ client/src/component/shoppingCart/productITem.js
@@ -1,93 +1,49 @@
 import React from 'react';
-import axios from 'axios'
-//import { getProducts} from './repository';
-
-//will be used to render each product on the product list.
 
 export default class ProductItem extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            products: {
-                bookTitle: "",
-                description: "",
-                bookCoverAddress: "",
-                price:"",
-                quantity: 1}
-            };
-            
-        }
-    
-    //     async componentDidMount() {
-    //     getProducts().then((products) => {
-    //       return this.setState({ products });
-    //     });
-    //   }
-    componentDidMount() {
-        this.getBook()
-       }
+	constructor(props) {
+		super(props);
+		this.state = {
+			quantity: 1
+		}
+	}
 
-  getBook(){
-     return axios.get('book/products',{
-        _id:"5e50b8101c9d4400000eed83"
-    })
-     .then(res=>{
-        console.log(res);
-         this.setState({products:res.data})
-        console.log(this.state);
-     })
-     .catch(err=>{
-         console.log(err);
-     });
-  }    
+	handleInputChange = event => this.setState({[event.target.name]: event.target.value})
 
-    // handleInputChange = event => 
-    // this.setState({[event.target.name]: event.target.value})
+	addToCart = () => {
+		let cart = localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : {};
+		let id = this.props.product.id.toString();
+		cart[id] = (cart[id] ? cart[id]: 0);
+		let qty = cart[id] + parseInt(this.state.quantity);
+		if (this.props.product.available_quantity < qty) {
+			cart[id] = this.props.product.available_quantity; 
+		} else {
+			cart[id] = qty
+		}
+		localStorage.setItem('cart', JSON.stringify(cart));
+	}
 
-    addToCart = () => {
-    let cart = localStorage.getItem('cart') 
-                    ? JSON.parse(localStorage.getItem('cart')) : {};
+	render(){
+		const { product } = this.props;
+		return (
+		    <div className="card" style={{ marginBottom: "10px"}}>
+			  <div className="card-body">
+			    <h4 className="card-title">{product.name}</h4>
+			    <p className="card-text">{product.description}</p>
+			    <h5 className="card-text"><small>price: </small>${product.price}</h5>
+			    <span className="card-text"><small>Available Quantity: </small>{product.available_quantity}</span>
+			    
+			    { product.available_quantity > 0 ?
+			    	<div>
+			    		<button className="btn btn-sm btn-warning float-right" onClick={this.addToCart}>Add to cart</button>
+			    		<input type="number" value={this.state.quantity} name="quantity" onChange={this.handleInputChange} className="float-right" style={{ width: "60px", marginRight: "10px", borderRadius: "3px"}}/>
+			    	</div> : 
+			    	<p className="text-danger"> product is out of stock </p>
+			 	}
 
-    let id = this.props.products.id.toString();
-    cart[id] = (cart[id] ? cart[id]: 0);
-    let qty = cart[id] + parseInt(this.state.quantity);
-    if (this.props.products.quantity < qty) {
-        cart[id] = this.props.products.quantity; 
-    } else {
-        cart[id] = qty
-    }
-    localStorage.setItem('cart', JSON.stringify(cart));
-    }
-
-    render(){
-    let products = [this.state.products];
-    
-    for (let key of Object.keys(products)) {
-        console.log(`$key: ${products[key]}`);
-    }
-    //console.log(products)
-    return (
-        <div className="card" style={{ marginBottom: "10px"}}>
-        <div className="card-body">
-            <h4 className="card-title">{products.bookTitle}</h4>
-            <p className="card-text">{products.description}</p>
-            <h5 className="card-text"><small>price: </small>${products.price}</h5>
-            <span className="card-text">
-            <small>Available Quantity: </small>{products.quantity}
-            </span>
-            { products.available_quantity > 0 ?
-            <div>
-                <button className="btn btn-sm btn-warning float-right" 
-                onClick={this.addToCart}>Add to cart</button>
-                <input type="number" value={this.state.quantity} name="quantity" 
-                onChange={this.handleInputChange} className="float-right" 
-                style={{ width: "60px", marginRight: "10px", borderRadius: "3px"}}/>
-            </div> : 
-            <p className="text-danger"> product is out of stock </p>
-        }
-        </div>
-    </div>
-    )
-    }
+			  </div>
+			</div>
+		)
+	}
 }
---
+++ server/data.js
@@ -0,0 +1,47 @@
+const products = [
+    {
+      id: 01,
+      name: 'Product 1',
+      available_quantity: 5,
+      price: 450,
+      description: 'Lorem ipsum dolor sit amet, iusto appellantur vix te, nam affert feugait menandri eu. Magna simul ad est. Nostrum neglegentur ius at, at pertinax repudiare vel. Vim an adolescens quaerendum.'
+    },
+  
+    {
+      id: 02,
+      name: 'Product 2',
+      available_quantity: 7,
+      price: 50,
+      description: 'Lorem ipsum dolor sit amet, iusto appellantur vix te, nam affert feugait menandri eu. Magna simul ad est. Nostrum neglegentur ius at, at pertinax repudiare vel. Vim an adolescens quaerendum.'
+    },
+  
+    {
+      id: 03,
+      name: 'Product 3',
+      available_quantity: 0,
+      price: 500,
+      description: 'Lorem ipsum dolor sit amet, iusto appellantur vix te, nam affert feugait menandri eu. Magna simul ad est. Nostrum neglegentur ius at, at pertinax repudiare vel. Vim an adolescens quaerendum.'
+    },
+  
+    {
+      id: 04,
+      name: 'Product 4',
+      available_quantity: 4,
+      price: 1500,
+      description: 'Lorem ipsum dolor sit amet, iusto appellantur vix te, nam affert feugait menandri eu. Magna simul ad est. Nostrum neglegentur ius at, at pertinax repudiare vel. Vim an adolescens quaerendum.'
+    },
+  ];
+
+  const users = [
+    {
+      'name': 'user',
+      'password': 'qwerty'
+    },
+    {
+      'name': 'example',
+      'password': 'qwerty'
+    }
+];
+
+  
+  module.exports = { 'products': products, users: users }
