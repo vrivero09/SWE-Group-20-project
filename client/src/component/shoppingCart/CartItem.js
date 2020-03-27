807aad7f437bdf7f413db2a81961d797877fb791
Vanessa Serret <rivero0288@outlook.com>
2020-03-27 15:28:37 -0400

    save button fixing

--- client/src/component/shoppingCart/CartItem.js
+++ client/src/component/shoppingCart/CartItem.js
@@ -10,16 +10,25 @@ export default class CartItem extends React.Component {
 		super(props);
 		this.state = {
 			quantity: 1,
-			products: []
 		}
 	}
 
 	handleInputChange = event => this.setState({[event.target.name]: event.target.value});
 
-	saveCartItem = (products) =>{
-		let save = localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : {};
-		localStorage.setItem('save', JSON.stringify(save));
-		this.setState({products})
+	saveCartItem = () =>{
+		let save = localStorage.getItem('save') ? JSON.parse(localStorage.getItem('save')) : {};
+        let productId = this.props.product._id.toString();
+        save[productId] = (save[productId] ? save[productId] : 0);
+        let qty = save[productId] + parseInt(this.state.quantity);
+        if (this.props.product.quantity < qty) {
+            save[productId] = this.props.product.quantity;
+        } else {
+            save[productId] = qty
+		}
+		let cart = localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : {};
+		localStorage.removeItem(cart)
+        localStorage.setItem('save', JSON.stringify(save));
+		
 	}
 
 
