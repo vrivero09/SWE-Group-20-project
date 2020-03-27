8f8f59d3d2dafa7664c9ca91bfeb8ce68887facc
Vanessa Serret <rivero0288@outlook.com>
2020-03-27 15:10:33 -0400

    more stuff on save function

--- client/src/component/shoppingCart/SavesItem.js
+++ client/src/component/shoppingCart/SavesItem.js
@@ -4,6 +4,7 @@ import {
   } from 'reactstrap';
 
 import {Link} from "react-router-dom";
+import { getSaveproducts } from './repository'
 
 
 export default class saveItem extends React.Component {
@@ -18,8 +19,8 @@ export default class saveItem extends React.Component {
 	//we’re using the setState method to keep the component’s state up-to-date every time a change occurs in our form
 	handleInputChange = event => this.setState({[event.target.name]: event.target.value})
 
-	addToCart = () => {
-        let cart = localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : {};
+	moveToCart = () => {
+		let cart = localStorage.getItem('save') ? JSON.parse(localStorage.getItem('save')) : {};
         let productId = this.props.product._id.toString();
         cart[productId] = (cart[productId] ? cart[productId] : 0);
         let qty = cart[productId] + parseInt(this.state.quantity);
@@ -28,9 +29,7 @@ export default class saveItem extends React.Component {
         } else {
             cart[productId] = qty
         }
-		localStorage.setItem('cart', JSON.stringify(cart));
-		localStorage.removeItem('save');
-
+        localStorage.setItem('cart', JSON.stringify(cart));
     }
 
 
@@ -43,7 +42,7 @@ export default class saveItem extends React.Component {
 				<Media src={product.bookCoverAddress} width="10%" alt="image holder" />
 			    <h5 className="card-title">{product.bookTitle}</h5>
 				<Button color="danger" size="md" onClick={() => this.props.remove(product)} style={{marginLeft: "85%"}}>Remove</Button>
-				<Link to="/Cart" className="btn btn-sm btn-warning float-right mx-3" onClick={this.addToCart}
+				<Link to="/Cart" className="btn btn-sm btn-warning float-right mx-3" onClick={this.moveToCart}
                     onChange={this.handleInputChange}>Move To cart</Link>
 
 				</Row>
