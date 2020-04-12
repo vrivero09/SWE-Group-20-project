b28c26bba11a314bba2beb194114d526be506951
alex154789 <49214518+alex154789@users.noreply.github.com>
2020-04-12 07:19:33 -0400

    added the bookcard and edited the list for sorting.

--- client/src/component/search/Sortbar.js
+++ client/src/component/search/Sortbar.js
@@ -13,6 +13,10 @@ function SortBar(props) {
                     <option value="publisher">Publisher</option>
                     <option value="price">Price</option>
                 </select>
+                <select onChange={props.changeAsc}>
+                    <option value="asc">Asc</option>
+                    <option value="des">Des</option>
+                </select>
             </div>
             <div>
                 Rating: &nbsp;
@@ -26,6 +30,13 @@ function SortBar(props) {
                 </select>
             </div>
             <div>
+                Books per page: &nbsp;
+                <select onChange={props.bpp}>
+                    <option value="10">10</option>
+                    <option value="20">20</option>
+                </select>
+            </div>
+            <div>
                 Price: &nbsp;
                 <select id="price" onChange={props.sortBooks}>
                     <option value="null">-------</option>
@@ -34,6 +45,17 @@ function SortBar(props) {
                     <option value="10-15">$10-$15</option>
                     <option value="15-20">$15-$20</option>
                     <option value="20+">$20+</option>
+                </select>
+            </div>
+            <div>
+                Filter By: &nbsp;
+                <select id="rating" onChange={props.filterBooks}>
+                    <option value="null">-------</option>
+                    <option value="top-seller">Top Sellers</option>
+                    <option value="Internet">Internet</option>
+                    <option value="Web Development">Web Dev</option>
+                    <option value="Java">Java</option>
+                    <option value="Miscellaneous">Misc</option>
                 </select>
             </div>
         </div>
