90265aaf765626f959c178c39a8e4527b610c612
vrivero09 <rivero0288@outlook.com>
2020-08-19 15:25:02 -0400

    added components for heroku

--- client/src/index.js
+++ client/src/index.js
@@ -1,20 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+
+import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
-// import cartReducer from './component/shoppingCart/reducer/cartReducer';
-// import { Provider } from 'react-redux';
-// import { createStore } from 'redux';
-
-//const store = createStore(cartReducer);
-
-//ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
-ReactDOM.render(<App />, document.getElementById('root'));
-
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+render((
+    <BrowserRouter>
+        <App/>
+    </BrowserRouter>
+), document.getElementById('root'));
