28d57de3ada74fe7620995011921c291df069c0d
vrivero09 <rivero0288@outlook.com>
2020-09-12 00:06:06 -0400

    commit

--- server/server.js
+++ server/server.js
@@ -102,8 +102,8 @@ if (process.env.NODE_ENV === 'production') {
 
 
   //Express serve up index.html file if it doesn't recognize route
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+  app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build/index.html'));
   });
 }
 
